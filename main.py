b3856b0c47f09ba794aa10ecb991ea23e18503fc
Krekun <baryonss@gmail.com>
2022-05-29 16:02:52 +0900

    Logファイルが中途半端だった場合の対策を追加

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import count
 import re
 import os
 from glob import glob
@@ -17,21 +18,45 @@ class Back_to_the_instance():
 
     #Get world id and instance in a log file
     def get_world(self):
-        with open(self.latest_file,"r",encoding="UTF") as f:
-            self.sentences=f.read()
-            m=re.finditer('Joining w.*\n',self.sentences)#Find worlds' and instances' id in a log fille
-            self.id_list=list(m)
-            self.id_list=list(map(lambda x:x.group().strip("Joining ").strip("\n"),self.id_list))
-            m=re.finditer('Entering Room.*\n',self.sentences)#Find worlds' names in a log fille
-            self.name_list=list(m)
-            self.name_list=list(map(lambda x:x.group().strip("Entering Room:").strip("\n"),self.name_list))
+        self.id_list=[]
+        self.name_list=[]
+        self.time_list=[]
+        count=0
+        for file in self.latest_file:
+            with open(file,"r",encoding="UTF") as f:
+                sentences=f.read()
+                self.get_id_list(sentences)
+                self.get_name_list(sentences)
+                self.get_time_list(sentences)
+            count+=1
+            if count>30:
+                break
+
+    def get_id_list(self,sentences):
+        m=re.finditer('Joining w.*\n',sentences)#Find worlds' and instances' id in a log fille
+        temp=list(m)
+        temp=list(map(lambda x:x.group().strip("Joining ").strip("\n"),temp))
+        self.id_list+=temp
+    
+    def get_name_list(self,sentences):
+        m=re.finditer('Entering Room.*\n',sentences)#Find worlds' names in a log fille
+        temp=list(m)
+        temp=list(map(lambda x:x.group().strip("Entering Room:").strip("\n"),temp))
+        self.name_list+=temp
+    def get_time_list(self,sentences):
+        m=re.finditer('.*Entering Room',sentences)#Find worlds' names in a log fille
+        temp=list(m)
+        temp=list(map(lambda x:str(x.group())[10:16],temp))
+        self.time_list+=temp
+
     #Get the latest log file of VRC
     #Log files of VRC is stored in \AppData\LocalLow\VRChat\VRChat
     #Names of log files is like output_log_15-24-16.txt
     def get_latest_log(self):
         self.list_of_files =glob(self.logpath+'*.txt')
         # self.latest_file = max(self.list_of_files, key=os.path.getctime)#Get the lates created log file 
-        self.latest_file = self.logpath+"output_log_11-09-11.txt"#Get the lates created log file 
+        self.latest_file = sorted(self.list_of_files, key=os.path.getctime, reverse=True)#Get the lates created log file 
+        # self.latest_file = [self.logpath+"output_log_11-09-11.txt"]#Get the lates created log file 
 
     def launch_game(self):
         id="vrchat://launch?id="+self.id_list[self.id_choice]
